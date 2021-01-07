f8289c7f16e6309f0b8283886558a40643e44634
Odunsi Joseph <odunsiolakunbi@gmail.com>
2021-01-07 21:39:48 +0100

    remove unused dependencies

--- client/src/api/axios.config.js
+++ client/src/api/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:9000/api',
+  baseURL: 'https://nameless-journey-88760.herokuapp.com/api',
   timeout: 1000
 });
 
--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const pool = require("./db");
 const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
 
 const PORT = process.env.PORT || 8080;
 
@@ -13,19 +11,10 @@ const product = require("./routes/product");
 const order = require("./routes/order");
 const cart = require("./routes/cart");
 const auth = require("./auth");
-const passport = require("passport");
 
 const app = express();
 
 app.use(express.json());
-
-app.use(
-  session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
 
 app.use(cors());
 
