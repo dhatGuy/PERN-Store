a9161aaa1a653ac9ef6e7f198bbe1b6a4559ebde
dhatGuy <odunsiolakunbi@gmail.com>
2021-01-25 18:28:09 +0100

    code splitting with lazy import

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,66 @@
-import ProductDetails from "pages/ProductDetails";
+import Spinner from "components/Spinner";
+import Layout from "layout/Layout";
+import Account from "pages/Account";
 import Cart from "pages/Cart";
 import Checkout from "pages/Checkout";
-import Home from "pages/Account";
-import Login from "pages/Login";
-import OrderDetails from "pages/OrderDetails";
-import Orders from "pages/Orders";
-import Product from "pages/ProductList";
-import Register from "pages/Register";
 import { ProtectedRoute } from "protected.route";
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+const ProductDetails = lazy(() => import("pages/ProductDetails"));
+const Login = lazy(() => import("pages/Login"));
+const OrderDetails = lazy(() => import("pages/OrderDetails"));
+const Orders = lazy(() => import("pages/Orders"));
+const Product = lazy(() => import("pages/ProductList"));
+const Register = lazy(() => import("pages/Register"));
+
 function App() {
-  
   return (
     <Router>
-      <>
-        <Switch>
-          <ProtectedRoute exact path="/profile">
-            <Home />
-          </ProtectedRoute>
-          <Route path="/signup">
-            <Register />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route exact path={["/", "/products"]}>
-            <Product />
-          </Route>
-          <ProtectedRoute exact path="/products/:id/">
-            <ProductDetails />
-          </ProtectedRoute>
-          <ProtectedRoute exact path="/checkout">
-            <Checkout />
-          </ProtectedRoute>
-          <ProtectedRoute exact path="/orders">
-            <Orders />
-          </ProtectedRoute>
-          <ProtectedRoute exact path="/orders/:id/">
-            <OrderDetails />
-          </ProtectedRoute>
-          <ProtectedRoute path="/cart">
-            <Cart />
-          </ProtectedRoute>
-          <Route path="*">
-            <h1>404 Error Found</h1>
-          </Route>
-        </Switch>
-      </>
-    </Router>
+      <Suspense fallback={
+        <Layout>
+
+        <div className="h-full flex items-center justify-center">
+          <Spinner size={150} />
+        </div>
+        </Layout>
+      }>
+        <>
+          <Switch>
+            <ProtectedRoute exact path="/profile">
+              <Account />
+            </ProtectedRoute>
+            <Route path="/signup">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route exact path={["/", "/products"]}>
+              <Product />
+            </Route>
+            <ProtectedRoute exact path="/products/:id/">
+              <ProductDetails />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/checkout">
+              <Checkout />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/orders">
+              <Orders />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/orders/:id/">
+              <OrderDetails />
+            </ProtectedRoute>
+            <ProtectedRoute path="/cart">
+              <Cart />
+            </ProtectedRoute>
+            <Route path="*">
+              <h1>404 Error Found</h1>
+            </Route>
+          </Switch>
+        </>
+    </Suspense>
+      </Router>
   );
 }
 
