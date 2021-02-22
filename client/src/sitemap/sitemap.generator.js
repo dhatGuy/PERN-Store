a1f1faaa0fc3ca45ff7401b4e339bf45c163a36e
dhatGuy <odunsiolakunbi@gmail.com>
2021-02-22 21:55:00 +0100

    generate sitemap

---
+++ client/src/sitemap/sitemap.generator.js
@@ -0,0 +1,16 @@
+require("babel-register")({
+  presets: ["es2015", "react"]
+});
+
+const router = require("./sitemap.routes").default;
+const Sitemap = require("react-router-sitemap").default;
+
+function generateSitemap () {
+    return (
+      new Sitemap(router)
+          .build("https://www.pern-store.netlify.app")
+          .save("./public/sitemap.xml")
+    );
+}
+
+generateSitemap();
