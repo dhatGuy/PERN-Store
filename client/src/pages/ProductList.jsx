2d16610b285f0931205d3249c63bead60493e597
dhatGuy <odunsiolakunbi@gmail.com>
2021-03-22 00:16:40 +0100

    refactor - add loading spinner on hompage while getting products

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -1,36 +1,38 @@
 import { Card, Pagination } from "@windmill/react-ui";
 import Product from "components/Product";
+import Spinner from "components/Spinner";
 import { useProduct } from "context/ProductContext";
 import Layout from "layout/Layout";
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ProductList = () => {
   const { products, setPage } = useProduct();
-  const history = useHistory();
 
   const handleChange = (page) => {
     setPage(page);
   };
 
-  const selectProduct = (id) => {
-    return history.push({
-      pathname: `products/${id}`,
-    });
-  };
+  if (!products) {
+    return (
+      <>
+        <Spinner size={100} loading />
+      </>
+    );
+  }
 
   return (
-    <Layout >
+    <Layout>
       <div className="container py-20 mx-auto">
         <Card className="flex flex-wrap h-full mx-2">
           {products?.map((prod) => (
-            <div
-              className="w-full flex flex-col justify-between sm:w-1/2 md:w-1/3 lg:w-1/4 my-2 px-2 box-border cursor-pointer"
+            <Link
+              className="w-full flex flex-col justify-between sm:w-1/2 md:w-1/3 lg:w-1/4 my-2 px-2 box-border"
               key={prod.product_id}
-              onClick={() => selectProduct(prod.product_id)}
+              to={`products/${prod.product_id}`}
             >
               <Product product={prod} />
-            </div>
+            </Link>
           ))}
         </Card>
         <a href="# ">
