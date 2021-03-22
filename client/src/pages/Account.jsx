109283b19736bc54bd2eac430a377db7542c0898
dhatGuy <odunsiolakunbi@gmail.com>
2021-03-22 22:31:41 +0100

    refactor - style profile page more

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,4 +1,4 @@
-import { Card, CardBody } from "@windmill/react-ui";
+import { Button } from "@windmill/react-ui";
 import { useUser } from "context/UserContext";
 import Layout from "layout/Layout";
 
@@ -7,16 +7,50 @@ const Account = () => {
 
   return (
     <Layout title="Profile" loading={userData === null}>
-      <div className="flex items-center justify-center">
-        <Card className="mt-2">
-          <CardBody>
-            <h1 className="text-4xl font-bold">Profile</h1>
-            <p className="my-6">Username: {userData?.username}</p>
-            <p className="my-6">Fullname: {userData?.fullname}</p>
-            <p className="my-6">Email: {userData?.email}</p>
-            <p>Roles: {userData?.roles.toString()}</p>
-          </CardBody>
-        </Card>
+      <div className="grid place-items-center pt-4 mt-10">
+        <div className="bg-white max-w-2xl shadow overflow-hidden sm:rounded-lg">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              Profile
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              Your personal information
+            </p>
+          </div>
+          <div className="border-t border-gray-200">
+            <dl>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Full name</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {userData?.fullname}
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Username</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {userData?.username}
+                </dd>
+              </div>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Email address
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {userData?.email}
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Roles</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {userData?.roles.toString()}
+                </dd>
+              </div>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <Button size="small">Edit</Button>
+              </div>
+            </dl>
+          </div>
+        </div>
       </div>
     </Layout>
   );
