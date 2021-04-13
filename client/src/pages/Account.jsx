0aa7d75be7ec6f559dcb7511b1b87c18bff28c9f
dhatGuy <odunsiolakunbi@gmail.com>
2021-04-14 00:44:18 +0100

    style - adjust account page on medium screen

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -34,7 +34,7 @@ const Account = () => {
         <AccountForm userData={userData} setShowSettings={setShowSettings} />
       ) : (
         <div className="grid place-items-center pt-4 mt-10">
-          <div className="w-full md:w-1/2 shadow-md overflow-hidden sm:rounded-lg">
+          <div className="w-full md:w-3/4 lg:w-1/2 shadow-md overflow-hidden sm:rounded-lg">
             <div className="px-4 py-5 sm:px-6">
               <h3 className="text-lg leading-6 font-medium text-gray-900">
                 Profile
