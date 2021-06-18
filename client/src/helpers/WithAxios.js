6ec5c5c1e65727906a7446bb098de12b4fc13d0f
Odunsi Joseph <odunsiolakunbi@gmail.com>
2021-06-18 19:09:53 +0100

    fix - refresh token only if user authenticated

--- client/src/helpers/WithAxios.js
+++ client/src/helpers/WithAxios.js
@@ -4,44 +4,46 @@ import { useUser } from "context/UserContext";
 import history from "helpers/history";
 
 const WithAxios = ({ children }) => {
-  const { setIsLoggedIn, setUserData, setAuthData } = useUser();
+  const { setIsLoggedIn, setUserData, setAuthData, isLoggedIn } = useUser();
 
   useMemo(() => {
-    API.interceptors.response.use(
-      (response) => response,
-      async (error) => {
-        const originalRequest = error.config;
-        if (
-          error.response.status === 401 &&
-          originalRequest.url === "/auth/refresh-token"
-        ) {
-          return new Promise((resolve, reject) => {
-            setIsLoggedIn(false);
-            setAuthData(null)
-            setUserData(null)
-            history.push("/login");
-            reject(error);
-          });
-        }
-
-        if (error.response.status === 401 && !originalRequest._retry) {
-          try {
-            originalRequest._retry = true;
-            const res = await API.post("/auth/refresh-token");
-            localStorage.setItem("token", JSON.stringify(res.data.token));
-            return API(originalRequest);
-          } catch (error) {
-            localStorage.removeItem("token");
-            setIsLoggedIn(false);
-            setAuthData(null);
-            setUserData(null);
-            history.push("/login");
+    if (isLoggedIn) {
+      API.interceptors.response.use(
+        (response) => response,
+        async (error) => {
+          const originalRequest = error.config;
+          if (
+            error.response.status === 401 &&
+            originalRequest.url === "/auth/refresh-token"
+          ) {
+            return new Promise((resolve, reject) => {
+              setIsLoggedIn(false);
+              setAuthData(null);
+              setUserData(null);
+              history.push("/login");
+              reject(error);
+            });
           }
+
+          if (error.response.status === 401 && !originalRequest._retry) {
+            try {
+              originalRequest._retry = true;
+              const res = await API.post("/auth/refresh-token");
+              localStorage.setItem("token", JSON.stringify(res.data.token));
+              return API(originalRequest);
+            } catch (error) {
+              localStorage.removeItem("token");
+              setIsLoggedIn(false);
+              setAuthData(null);
+              setUserData(null);
+              history.push("/login");
+            }
+          }
+          return Promise.reject(error);
         }
-        return Promise.reject(error);
-      }
-    );
-  }, [setAuthData, setIsLoggedIn, setUserData]);
+      );
+    }
+  }, [isLoggedIn, setAuthData, setIsLoggedIn, setUserData]);
 
   return children;
 };
