bb2379773825f75f14ade59305eec7861eb7ee34
dhatguy <odunsiolakunbi@gmail.com>
2021-12-24 16:39:27 +0100

    fix: prevent text copy on product card

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -2,42 +2,45 @@ import { Button, CardBody } from "@windmill/react-ui";
 import { useCart } from "context/CartContext";
 import React from "react";
 import { ShoppingCart } from "react-feather";
+import { Link } from "react-router-dom";
 import { formatCurrency } from "../helpers/formatCurrency";
 
 const Product = ({ product }) => {
   const { addItem } = useCart();
 
   const addToCart = async (e) => {
-    e.stopPropagation();
+    e.preventDefault();
     await addItem(product, 1);
   };
   return (
-    <div className="group">
-      <span className="block relative h-48 rounded overflow-hidden">
-        <img
-          className="w-full h-full object-contain object-center"
-          src={product.image_url}
-          alt={product.name}
-          loading="lazy"
-          decoding="async"
-          title={product.name}
-        />
-      </span>
-      <CardBody className="flex flex-col items-stretch mt-4">
-        <h2 className="title-font text-lg font-medium overflow-ellipsis whitespace-nowrap overflow-hidden">
-          {product.name}
-        </h2>
-        <p className="">{formatCurrency(product.price)}</p>
-        <Button
-          iconLeft={ShoppingCart}
-          className="mt-4 transition duration-200 ease-out lg:bg-opacity-0 group-hover:bg-opacity-100"
-          onClick={(e) => addToCart(e)}
-        >
-          {" "}
-          Add to cart
-        </Button>
-      </CardBody>
-    </div>
+    <Link to={`/products/${product.product_id}`}>
+      <div className="group">
+        <span className="block relative h-48 rounded overflow-hidden">
+          <img
+            className="w-full h-full object-contain object-center"
+            src={product.image_url}
+            alt={product.name}
+            loading="lazy"
+            decoding="async"
+            title={product.name}
+          />
+        </span>
+        <CardBody className="flex flex-col items-stretch mt-4">
+          <h2 className="title-font text-lg font-medium overflow-ellipsis whitespace-nowrap overflow-hidden">
+            {product.name}
+          </h2>
+          <p className="">{formatCurrency(product.price)}</p>
+          <Button
+            iconLeft={ShoppingCart}
+            className="mt-4 transition duration-200 ease-out lg:bg-opacity-0 group-hover:bg-opacity-100"
+            onClick={(e) => addToCart(e)}
+          >
+            {" "}
+            Add to cart
+          </Button>
+        </CardBody>
+      </div>
+    </Link>
   );
 };
 
--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -4,18 +4,14 @@ import Spinner from "components/Spinner";
 import { useProduct } from "context/ProductContext";
 import Layout from "layout/Layout";
 import React from "react";
-import { useHistory } from "react-router-dom";
 
 const ProductList = () => {
   const { products, setPage } = useProduct();
-  const history = useHistory();
 
   const handleChange = (page) => {
     setPage(page);
     window.scrollTo({ behavior: "smooth", top: 0 });
   };
-
-  const toProduct = (product_id) => history.push(`products/${product_id}`);
 
   if (!products) {
     return (
@@ -35,7 +31,6 @@ const ProductList = () => {
             <div
               className="w-full flex flex-col justify-between sm:w-1/2 md:w-1/3 lg:w-1/4 my-2 px-2 box-border"
               key={prod.product_id}
-              onClick={() => toProduct(prod.product_id)}
             >
               <Product product={prod} />
             </div>
