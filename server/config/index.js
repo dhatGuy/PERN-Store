df3601beb0eb2aec505b3de1c87bb6b69ddfd72f
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-12-30 09:09:09 +0100

    remove env DATABASE_URL

--- server/config/index.js
+++ server/config/index.js
@@ -8,9 +8,8 @@ const database =
     : process.env.POSTGRES_DB;
 
 const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${database}`;
-console.log(connectionString);
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  connectionString,
   /*
     SSL is not supported in development
     */
