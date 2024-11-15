4019dcfc8e5b9bf282ddc6869022020e426c8181
Navi <navi@localhost.localdomain>
2024-11-15 14:04:55 +0530

    variablise existing configuration

--- Deployment/Terraform/modules/network/main.tf
+++ Deployment/Terraform/modules/network/main.tf
@@ -1,63 +1,29 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
-# VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "vpc-for-private-ec2"
+    Name = "VPC_PERN_Store"
   }
 }
 
-# Public Subnets
-resource "aws_subnet" "public_az_1a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+resource "aws_subnet" "subnets" {
+  for_each             = var.subnets
+  vpc_id               = aws_vpc.main.id
+  cidr_block           = each.value.cidr_block
+  map_public_ip_on_launch = each.value.map_public_ip
+  availability_zone    = each.value.availability_zone
 
   tags = {
-    Name = "public-az-1a"
+    Name = each.value.name
   }
 }
 
-resource "aws_subnet" "public_az_1b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1b"
-
-  tags = {
-    Name = "public-az-1b"
-  }
-}
-
-# Private Subnets
-resource "aws_subnet" "private_az_1a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "private-az-1a"
-  }
-}
-
-resource "aws_subnet" "private_az_1b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "private-az-1b"
-  }
-}
-
-# Internet Gateway for Public Subnets
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -66,7 +32,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-# Elastic IP for NAT Gateway
+
 resource "aws_eip" "nat_eip" {
   domain = "vpc"
   
@@ -75,26 +41,22 @@ resource "aws_eip" "nat_eip" {
   }
 }
 
-# NAT Gateway for Private Subnets
+
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_az_1a.id # NAT Gateway in Public Subnet
+  subnet_id     = aws_subnet.subnets["public_az_1a"].id
+
+  tags = {
+    Name = "nat-gateway"
+  }
 }
 
-# Route Table for Public Subnets
+
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
-
-  # Default route to the internet via Internet Gateway
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
-  }
-
-  # Local route for VPC CIDR block (public subnet)
-  route {
-    cidr_block = "10.0.0.0/16"
-    gateway_id = "local"
   }
 
   tags = {
@@ -102,31 +64,11 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-# Associate Public Route Table with Public Subnets
-resource "aws_route_table_association" "public_add_az_1a" {
-  subnet_id      = aws_subnet.public_az_1a.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "public_add_az_1b" {
-  subnet_id      = aws_subnet.public_az_1b.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-# Route Table for Private Subnets
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
-
-  # Route to the internet via NAT Gateway
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gw.id
-  }
-
-  # Local route for VPC CIDR block (private subnet)
-  route {
-    cidr_block = "10.0.0.0/16"
-    gateway_id = "local"
   }
 
   tags = {
@@ -134,13 +76,16 @@ resource "aws_route_table" "private_rt" {
   }
 }
 
-# Associate Private Route Table with Private Subnets
-resource "aws_route_table_association" "private_add_az_1a" {
-  subnet_id      = aws_subnet.private_az_1a.id
-  route_table_id = aws_route_table.private_rt.id
+
+resource "aws_route_table_association" "public" {
+  for_each = { for k, v in var.subnets : k => v if v.map_public_ip }
+  subnet_id      = aws_subnet.subnets[each.key].id
+  route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_route_table_association" "private_add_az_1b" {
-  subnet_id      = aws_subnet.private_az_1b.id
+
+resource "aws_route_table_association" "private" {
+  for_each = { for k, v in var.subnets : k => v if !v.map_public_ip }
+  subnet_id      = aws_subnet.subnets[each.key].id
   route_table_id = aws_route_table.private_rt.id
 }
---
+++ Deployment/Terraform/modules/network/variables.tf
@@ -0,0 +1,40 @@
+variable "region" {
+  type = string
+  description = "default region will be taken "
+  default = "us-east-1"
+}
+
+variable "subnets" {
+  type = map(object({
+    cidr_block        = string
+    map_public_ip     = bool
+    availability_zone = string
+    name              = string
+  }))
+  default = {
+    "public_az_1a" = {
+      cidr_block        = "10.0.1.0/24"
+      map_public_ip     = true
+      availability_zone = "us-east-1a"
+      name              = "public-az-1a"
+    }
+    "public_az_1b" = {
+      cidr_block        = "10.0.2.0/24"
+      map_public_ip     = true
+      availability_zone = "us-east-1b"
+      name              = "public-az-1b"
+    }
+    "private_az_1a" = {
+      cidr_block        = "10.0.3.0/24"
+      map_public_ip     = false
+      availability_zone = "us-east-1a"
+      name              = "private-az-1a"
+    }
+    "private_az_1b" = {
+      cidr_block        = "10.0.4.0/24"
+      map_public_ip     = false
+      availability_zone = "us-east-1b"
+      name              = "private-az-1b"
+    }
+  }
+}
