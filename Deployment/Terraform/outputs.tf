5f7f0c64f72035839057fa4072cff81117f71884
navigill7 <nnavigill784@gmail.com>
2024-11-18 20:51:05 +0530

    added the out block for sonar_lb arn

--- Deployment/Terraform/outputs.tf
+++ Deployment/Terraform/outputs.tf
@@ -5,3 +5,8 @@ output "public_ip_Basiton_host" {
 output "Basiton_Instance_Id" {
   value = module.network.Basiton_Instance_Id
 }
+
+output "sonar_lb_dns" {
+  value = module.load_balancer.sonar_lb_dns
+}
+
