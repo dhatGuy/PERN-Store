751dbcc8ccf43340b0ec268470347354e4af396c
navigill7 <nnavigill784@gmail.com>
2024-11-27 22:53:39 +0530

    adding security group for nexus

--- Deployment/Terraform/modules/security/main.tf
+++ Deployment/Terraform/modules/security/main.tf
@@ -128,6 +128,38 @@ resource "aws_security_group" "sonar_alb_sg" {
 }
 
 
+resource "aws_security_group" "nexus_sg" {
+  name = "${var.environment}-nexus-sg"
+  vpc_id = var.vpc_id
+  description = "allowing integration from jenkins"
+
+  dynamic "ingress" {
+    for_each = var.nexus_ingress_rules
+    content {
+      from_port = ingress.value.from_port
+      to_port = ingress.value.to_port
+      protocol = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_block
+    }
+
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.environment}-nexus-sg"
+  }
+
+
+}
+
+
 
   
 
